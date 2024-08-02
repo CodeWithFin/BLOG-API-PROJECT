94154228e148008b4d3f83aacf072238f532b006
Finley <finley.mwachia12@gmail.com>
2024-08-02 09:00:00 +0300

    add get response to fetch specific post in blog api

--- index.js
+++ index.js
@@ -45,7 +45,15 @@ app.get("/posts",(req,res)=>{
   res.json(posts);
 })
 //CHALLENGE 2: GET a specific post by id
-
+app.get("/posts/:id",(req,res)=>{
+  const id = parseInt(req.params.id, 10);
+  const post = posts.find((p)=>p.id === id);
+  if(post){
+    res.json(post);
+  } else{
+    res.status(404).send({message : "Post not Found"});
+  }
+});
 //CHALLENGE 3: POST a new post
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
